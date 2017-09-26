5905d4d646c5b59cdd860578527d0305bac51909
KojiKobayashi <k.kobayashi@33i.co.jp>
2017-09-26 15:14:17 +0900

    pep8対応

--- src/two_window_viewer.py
+++ src/two_window_viewer.py
@@ -5,6 +5,7 @@ import numpy as np
 import tkinter
 import tkinter.filedialog
 
+
 class TwoImages:
     def __init__(self):
         self.mode = 0
@@ -12,6 +13,7 @@ class TwoImages:
         self.window_name = "compare"
         self.align = "h"
         self.enlarge_rate = 100
+
     def set_files(self, files):
         self.imgs = [cv2.imread(f.encode("shift_jis").decode("utf-8")) for f in files]
         self.filename = files[0].split("\\")[-1]
@@ -24,35 +26,47 @@ class TwoImages:
             max_w = max([img.shape[1] for img in self.imgs])
             show_imgs = [self._resize_w(img, max_w) for img in self.imgs]
             self.concat = cv2.vconcat(show_imgs)
+
     def show_image(self):
         message = self._set_message()
         width = self.concat.shape[1] * self.enlarge_rate // 100
         height = self.concat.shape[0] * self.enlarge_rate // 100
         disp_img = cv2.resize(self.concat, (width, height))
-        cv2.putText(disp_img, message, (0,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,255), 3)
+        cv2.putText(disp_img, message, (0, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0 ,255), 3)
         cv2.imshow(self.window_name, disp_img)
+
     def set_identical_mode(self):
         self.mode = 1
+
     def set_show_filename_mode(self):
         self.mode = 2
+
     def save(self):
         cv2.imwrite(self.filename, self.concat)
         self.tmp_message = "saved"
         self.once_message_flag = True
+
     def reset_window_position(self):
         cv2.moveWindow(self.window_name, 0, 0)
+
     def set_align_vertical(self):
         self.align = "v"
+
     def set_align_horizontal(self):
         self.align = "h"
+
     def enlarge_image(self):
         self.enlarge_rate = min(self.enlarge_rate + 10, 500)
+
     def decrease_image(self):
         self.enlarge_rate = max(self.enlarge_rate - 10, 10)
+
     def reset_window_message(self):
-    	self.mode = 0
+        self.mode = 0
+
     def are_same_images(self):
         return self._are_same_images()
+
     def _set_message(self):
         message = ""
         if self.tmp_message == "":
@@ -64,31 +78,36 @@ class TwoImages:
             message = self.tmp_message
             self.tmp_message = ""
         return message
+
     def _are_same_images(self):
         for img in self.imgs:
-            if not np.array_equal(self.imgs[0],img):
+            if not np.array_equal(self.imgs[0], img):
                 return "NOT SAME"
         return "SAME"
+
     def _resize_w(self, img, w):
         h = img.shape[0] * w // img.shape[1]
         return cv2.resize(img, (w, h))
+
     def _resize_h(self, img, h):
         w = img.shape[1] * h // img.shape[0]
         return cv2.resize(img, (w, h))
-    def __del__( self ):
+
+    def __del__(self):
         cv2.destroyAllWindows()
+
 
 def show_two_images(files, dirs):
     length = len(files)
     counter = 0
-    
+
     two_images = TwoImages()
     while(True):
         f = files[counter]
         filePaths = [os.path.join(dir, f) for dir in dirs]
         two_images.set_files(filePaths)
         two_images.show_image()
-    
+
         key = cv2.waitKey(0)
 
         if key == 27:       # esc
@@ -117,20 +136,20 @@ def show_two_images(files, dirs):
             two_images.reset_window_message()
         elif key == ord('j'):
             if two_images.are_same_images() == "NOT SAME":
-                while(key ==ord('j')):
+                while(key == ord('j')):
                     key = cv2.waitKey(100)
             else:
                 counter += 1
-                    
 
         if counter >= length:
             counter = length - 1
         if counter < 0:
             counter = 0
-    
+
 #        print key
-        
+
     cv2.destroyAllWindows()
+
 
 def uniq_arrays(arrays):
     ret_arr = []
@@ -138,13 +157,15 @@ def uniq_arrays(arrays):
         if all(item in arr for arr in arrays):
             ret_arr.append(item)
     return ret_arr
-            
+
+
 def get_image_files(dir):
     files = []
     for ext in ['png', 'jpg', 'jpeg', 'bmp', 'gif']:
         files += glob.glob(dir + '\*.' + ext)
     files = [f.split("\\")[-1] for f in files]
     return files
+
 
 def set_n_directory():
     # root setting to prepend blank window
@@ -154,7 +175,6 @@ def set_n_directory():
     dir_opt = options = {}
     while True:
         tmp = tkinter.filedialog.askdirectory(**dir_opt)
-        tmo = tmp.encode("utf-8")
         if not tmp:
             break
         dirs.append(tmp)
@@ -162,6 +182,7 @@ def set_n_directory():
 
     root.destroy()
     return dirs
+
 
 def main_exe():
     dirs = set_n_directory()
@@ -173,7 +194,7 @@ def main_exe():
 
     show_two_images(files, dirs)
 
+
 if __name__ == "__main__":
     main_exe()
 
-    
